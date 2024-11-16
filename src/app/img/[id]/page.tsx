3ee31c8cbf90ad9e9c525e8229500f818a64ad12
Alexander <alexandergaruba96@gmail.com>
2024-11-16 17:35:42 +0100

    refactor image modal implementation; replace image rendering with FullPageImageView component and enhance modal styling

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,16 +1,13 @@
-import { getImage } from "~/server/queries";
+import { FullPageImageView } from "~/common/full-page-image-view";
 
-export default async function PhotoModal({
+export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = Number(photoId);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-  const image = await getImage(idAsNumber);
   return (
-    <div>
-      <img src={image.url} className="w-96" />
+    <div className="flex h-full min-h-0 w-full min-w-0 overflow-y-hidden">
+      <FullPageImageView photoId={photoId} />
     </div>
   );
 }
